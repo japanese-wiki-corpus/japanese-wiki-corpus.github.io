6e7f32b439966c10e1919a31976807f20d2643aa
A. C. Yu <amyyu123@hotmail.com>
2020-06-14 22:46:19 -0700

    get keywords and format search results page

--- src/genKeywords.py
+++ src/genKeywords.py
@@ -1,42 +1,84 @@
 import sys
 import os
-import xml.etree.ElementTree as ET
 import json
 
 testing = False
 
 cats = ['Buddhism', 'building', 'culture', 'emperor', 'family', 'geographical', 'history', 'literature', 'person', 'railway', 'road', 'shrines', 'school', 'Shinto', 'title']
 if testing:
-	cats = ['school']
+	cats = ['Buddhism']
 
 dataPath = '../japanese_wiki_corpus_data/'
 
+illegal = ['(', '[']
+
+synonyms = {'shisen-gumi': 'shinsengumi', 'sannan': 'yamanami', 'sannan keisuke': 'yamanami keisuke'}
+
 def filename2keyword(file):
 	keyword = os.path.splitext(file)[0]
-	keyword = re.sub("[\(\[].*?[\)\]]", "", keyword)
+	
+	for c in illegal:
+		i = keyword.find(c)
+		if i != -1:
+			keyword = keyword[:i]
+	
 	keyword = keyword.strip(" .")
 	return keyword
 
-def getPageRank(content):
+def getNLinks(content):
 	start = 0
-	links = {}
-    while True:
-        start = content.find('<a href=', start)
-        if start == -1 or start > len(content): return
-		
+	links = set([])
+	while True:
+		start = content.find('<a href=', start)
+		if start == -1 or start > len(content): break
+
 		i1 = content.find('>', start) + 1
 		i2 = content.find('</a>', start)
 		links.add(content[i1:i2])
 
-        start += 1
+		start += 1
 
 	return len(links)
 
+def orderName(name):
+	words = name.split(' ')
+	if words[-1].isupper():
+		words.insert(0, words.pop())
+	return ' '.join(words)
+
 def getLastname(kw):
 	words = kw.split(' ')
-	if words[-1].isupper():
-		return words[-1]
+	if words[0].isupper():
+		return words[0].lower()
 	return None
+
+def getPageRank(keywords):
+	nproc = 0
+	ranks = {}
+	for cat in cats:
+		print(cat, flush=True)
+		files = os.listdir(cat)
+		for file in files:
+			if testing:
+				file = 'Ashura.html' #testing
+			filepath = cat+"/"+file
+			inp = open(dataPath+filepath, "r", encoding="utf8")
+			content = inp.read()
+			content = content.lower()
+			
+			for kw in keywords:
+				if kw not in ranks:
+					ranks[kw] = 0
+				ranks[kw] += content.count(' '+kw+' ')
+				ranks[kw] += content.count('>'+kw+'</a>')
+			
+			inp.close()
+			nproc+=1
+			if nproc % 100 == 0:
+				print(nproc, flush=True)
+			if testing:
+				break
+	return ranks
 
 def run():
 	keywords = {}
@@ -50,23 +92,41 @@ def run():
 			inp = open(dataPath+filepath, "r", encoding="utf8")
 			content = inp.read()
 			
-			kw = filename2keyword(file)
-			rank = getPageRank(content)
+			kw = orderName(filename2keyword(file))
+			if len(kw) < 3:
+				continue
+			
+			nlinks = getNLinks(content)
 			
 			lastname = getLastname(kw)
 			kw = kw.lower()
 			if kw not in keywords:
 				keywords[kw] = 1
-			keywords[kw] += rank
+			keywords[kw] += nlinks
 			
 			if lastname:
-				if lastname not in keywords:
-					keywords[lastname] = 2
-				keywords[lastname] += rank
+				if len(lastname) < 3:
+					continue
+				if lastname not in keywords or keywords[lastname] <= nlinks:
+					keywords[lastname] = nlinks+2
+
 			inp.close()
-			
 			if testing:
 				break
+	
+	if not os.path.isfile(rank.json):
+		ranks = getPageRank(keywords)
+		with open('rank.json', 'w', encoding="utf8") as outfile:
+			json.dump(ranks, outfile, ensure_ascii=False)
+	else:
+		with open('rank.json', encoding='utf8') as f:
+			ranks = json.load(f)
+
+	for kw in keywords.keys():
+		keywords[kw] = int(keywords[kw]*0.5 + ranks[kw]*0.5)
+		
+	for kw in synonyms.keys():
+		keywords[synonyms[kw]] = keywords[kw] + 1
 	
 	nkw = len(keywords)
 	print(nkw)
