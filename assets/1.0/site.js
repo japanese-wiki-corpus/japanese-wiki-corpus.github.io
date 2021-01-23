fec79da292a1b12c7aaf90897c5ff3b9119e021c
A. C. Yu <amyyu123@hotmail.com>
2021-01-22 23:34:46 -0800

    make only main text quotable

--- assets/1.0/site.js
+++ assets/1.0/site.js
@@ -40,17 +40,3 @@ window.__gcse = {
   parsetags: 'onload',
   initializationCallback: gcseInitCallback
 };
-
-$(window).on('load', function(){
-	
-// Facebook buttons
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-	js.async=true;
-    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  
-});
