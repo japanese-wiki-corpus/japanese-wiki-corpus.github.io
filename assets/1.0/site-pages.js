b1690ca7ebff7d9768c4b581bcc044d6f0d200c5
A. C. Yu <amyyu123@hotmail.com>
2021-02-17 02:39:58 -0800

    custom ad

--- assets/1.0/site-pages.js
+++ assets/1.0/site-pages.js
@@ -1,5 +1,10 @@
 $(window).on('load', function(){
 
+console.log($('.ap_container').height());
+  if ($('.ap_container').height() > 10) { 
+	$('.ap_container').attr('style', 'border-style: solid; border-width: medium; width: fit-content; margin: 40 0;');
+  }
+
 // Facebook buttons
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -9,7 +14,7 @@ $(window).on('load', function(){
     js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
-  
+
 });
 
 if (document.getElementById("main-text").scrollHeight - 100 <= document.body.clientHeight) {
